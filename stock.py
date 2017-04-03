a696a39c48d7c175ea6cfe84e76b50de33ddbb2d
Adrian Cann <adriantcann@gmail.com>
2017-04-03 19:39:09 -0400

    Iterate through files and parse html for D/E ratio
    * These few lines go through the html files and split on the element
    around the Debt/Equity ratio. Printing these values shows that some may
    be N/A some may be `0` and the rest are in decimal form e.g., `0.407`
    * Yahoo does not change the markup much thankfully
    
    https://www.youtube.com/watch?v=2vQfMAEu670&index=5&list=PLQVvvaa0QuDd0flgGphKCej-9jp-QdzZ3

--- stock.py
+++ stock.py
@@ -13,12 +13,23 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
     for each_dir in stock_list[1:]:
     # starting at 1 because it lists the root directory first
         each_file = os.listdir(each_dir)
+
+        ticker = each_dir.split("/")[-1]
+        # company abreviation 
+
         if len(each_file) > 0:
             # empty file
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
-                print(date_stamp, unix_time)
-                time.sleep(15)
+
+                full_file_path = each_dir + "/" + file
+                #print(full_file_path)
+                source = open(full_file_path, "r").read()
+                #print(source)
+                value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split("</td>")[0]
+
+                print(ticker + ": " + value)
+            time.sleep(12)
 
 Key_Stats()
